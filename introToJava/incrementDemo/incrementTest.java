e4bf3742a893855ec19fc44069a0b1fa77f26620
r-kill <rowan.kill@live.com>
2019-02-01 14:56:11 -0600

    Create incrementTest.java

---
+++ introToJava/incrementDemo/incrementTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rowan
+ */
+public class PrePostIncrement {
+
+    public static void main(String[] args) {
+        int c = 5;
+
+        System.out.println(c);
+        System.out.println(c++);
+        System.out.println(c);
+
+        System.out.println();
+
+        c = 5;
+        System.out.println(c);
+        System.out.println(++c);
+        System.out.println(c);
+    }//end Main
+}//end Class
