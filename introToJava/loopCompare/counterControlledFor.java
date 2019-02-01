eff98c0098db6b55128d2c546e2ddb300f6786ed
r-kill <rowan.kill@live.com>
2019-02-01 15:02:10 -0600

    Create counterControlledFor.java

---
+++ introToJava/loopCompare/counterControlledFor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rowan
+ */
+public class CounterControlledFor {
+
+    public static void main(String[] args) {
+        for (int count = 1; count <= 10; count++) {
+            System.out.printf("%d ", count);
+        }
+        System.out.println();
+    }//end Main
+}//end Class
